3deb80d52077783aab5bd6771767a11d6ef8783f
Gonzalo Arroyo Perez <gonzalo.arroyo@opendeusto.es>
2024-04-03 19:06:15 +0200

    Main actualizacion

--- hotel/main.c
+++ hotel/main.c
@@ -1,8 +1,30 @@
-/*
- * main.c
- *
- *  Created on: 2 abr 2024
- *      Author: Eneko
- */
+
+int main(void){
+
+	int seleccion;
+
+	printf("== Menu Principal==\n");
+	printf("1- Iniciar Sesion\n");
+	printf("2- Registrarse \n");
+	printf("3- Salir \n");
+
+	scanf("%d",&seleccion);
+
+	switch(seleccion){
+
+	case 1:
+		printf("==");
+	}
+}
+
+void iniciarSesion(){
+	int seleccion;
 
 
+	printf("== Iniciar Sesion==\n");
+	printf("Introduce el nombre de usuario :\n");
+	printf("Introduce la contraseña:n");
+	printf("0. Atras \n");
+
+
+}
