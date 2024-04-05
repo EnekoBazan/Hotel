611f410f4f70139a6e845bfc71ec77bfc07d2ca0
UnaiiGonzalezz <116153554+UnaiiGonzalezz@users.noreply.github.com>
2024-04-05 13:02:42 +0200

    habitacion.c imprimirHab

--- hotel/habitacion.c
+++ hotel/habitacion.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include "Habitacion.h"
+#include <string.h>
+#include <stdlib.h>
 
-
-
+void imprimirHabitacion(Habitacion h){
+	printf("Habitacion nº: %i, Tipo de habitacion: %s, Capacidad: %i, Precio por noche: %.2f, Estado: %s", h.id, h.tipo, h.capacidad, h.precio, h.estado);
+}
