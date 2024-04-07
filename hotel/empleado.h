388daa546d141fd041cb194ead5b6af20ef233eb
UnaiiGonzalezz <116153554+UnaiiGonzalezz@users.noreply.github.com>
2024-04-07 13:48:45 +0200

    empleado.h

---
+++ hotel/empleado.h
@@ -0,0 +1,23 @@
+#ifndef EMPLEADO_H_
+#define EMPLEADO_H_
+
+#include "../sqlite3.h"
+
+typedef struct{
+	int id;
+	char* nombre;
+	char* apellido;
+	float salario;
+	char* puesto;
+	int telefono;
+	char* sexo;
+	char* nombreUsuario;
+	char* contrasena;
+}Empleado;
+
+void imprimirEmpleado(Empleado e);
+
+int loginEmpleado(Empleado* empleados, int tamanyo);
+
+
+#endif /* EMPLEADO_H_ */
